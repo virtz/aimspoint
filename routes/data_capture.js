39ebd7243ce1e9b1ff61d1a25de8ba27bb00a525
Virtuoso O <80458276+odunayoojomo@users.noreply.github.com>
2021-10-01 15:58:57 +0100

    Added extra route

--- routes/data_capture.js
+++ routes/data_capture.js
@@ -150,6 +150,64 @@ router.post('/fetch',auth,validator(validateInput),async(req,res,next)=>{
     });
 });
 
+router.post('/findCopy',auth,validator(validateInput2),async(req,res,next)=>{
+    sql.connect(serverconfig).then(()=>{
+        // connect();
+        var request = new sql.Request();
+        request
+        .input('Client',sql.VarChar(50),req.body.client)
+        .input('Barcode',sql.VarChar(50),req.body.barcode)
+        .query(`SELECT [ID]
+        ,[Product]
+        ,[Location]
+        ,[Barcode]
+        ,[Year]
+        ,[DateCaptured]
+        ,[LastUpdated]
+        ,[CapturedBy]
+        ,[UpdatedBy]
+        ,[SerialNo]
+        ,[Condition]
+        ,[Message]
+        ,[SiteName]
+        ,[SiteAddress]
+        ,[CostCenter]
+        ,[Latitude]
+        ,[Longitude]
+        ,[MapShape]
+        ,[Comment]
+        ,[Status]
+        ,[Client]
+        ,[IsParent]
+        ,[ParentBarcode]
+        ,[Person]
+        ,[BrExtra1]
+        ,[BrExtra2]
+        ,[Drop1]
+        ,[Drop2]
+        ,[Drop3]
+        ,[Text1]
+        ,[Text2]
+        ,[Text3]
+        ,[Text4]
+        ,[Text5]
+        ,[Text6]
+        ,[Text7]
+        ,[Text8]
+        ,[Mode]
+    FROM [dbo].[Aims_DataCapture] where client=@Client and barcodde=@Barcode`,function(err,data){
+            if (err) console.log(err)
+            var resultLength = Object.values(data.recordset).length;
+            if (resultLength == 0)
+            return res.status(404).json({'error':'Data not found'})
+
+            res.send(data.recordset);
+        });
+    }).catch((err)=>{
+        next(err);
+    });
+});
+
 
 
 function validate(req){
@@ -209,6 +267,16 @@ function validateInput(req){
     const schema = Joi.object({
         user:Joi.string().min(3).max(300).required(),
         location:Joi.string().min(3).max(300).required(),
+        client:Joi.string().min(3).max(300).required(),
+
+    });
+
+    return schema.validate(req);
+}
+
+function validateInput2(req){
+    const schema = Joi.object({
+        barcode:Joi.string().min(3).max(300).required(),
         client:Joi.string().min(3).max(300).required(),
 
     });
