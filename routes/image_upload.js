580c0244c9fbb2256638bba8fd372233f64cfcd1
Virtuoso O <80458276+odunayoojomo@users.noreply.github.com>
2021-10-08 12:33:41 +0100

    this is  it

--- routes/image_upload.js
+++ routes/image_upload.js
@@ -71,13 +71,13 @@ form.on('error', function(error) {
     next(error);
 });
 
-// form.on('close', function() {
-//    res.send('received upload');
-// });
+form.on('close', function(name,file) {
+   res.send({"message":"Uplaod successful","path":file.path});
+});
 
-form.on(file,function(name,file){
-    res.send(file.path);
-})
+// form.on(file,function(name,file){
+//     res.send(file.path);
+// })
 form.parse(req, function(err, fields, files) {
     res.writeHead(200, {'content-type': 'text/plain'});
     res.write('received upload:\n\n');
