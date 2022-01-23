e16132da3a91ddee5f2ee15b14bcd3e4d3847640
Virtuoso O <odunayoojomo0@gmail.com>
2022-01-23 12:26:33 +0100

    data audit

--- routes/data_audit.js
+++ routes/data_audit.js
@@ -6,21 +6,21 @@ const auth = require('../middleware/auth');
 const validator = require('../middleware/validate');
 const Joi = require('joi');
 
-router.post('/', auth, validator(validate), async (req, res, next) => {
+router.post('/', validator(validate), async (req, res, next) => {
     sql.connect(serverconfig).then(() => {
         // connect();
         var request = new sql.Request();
 
         request
-            .input('Barcode', sql.VarChar(200), req.body.client)
+            .input('Barcode', sql.VarChar(200), req.body.barcode)
             .query(`SELECT [ID] ,[Product],[Location],[Barcode],[Year],[DateCaptured],[LastUpdated],[CapturedBy],[UpdatedBy],[SerialNo] ,[Condition] ,[Message],[SiteName],[SiteAddress],[CostCenter],[Latitude],[Longitude] ,[MapShape],[Comment],[Status],[Client],[IsParent],[ParentBarcode],[Person],[BrExtra1]
-        ,[BrExtra2],[Drop1],[Drop2],[Drop3],[Text1],[Text2],[Text3],[Text4],[Text5],[Text6],[Text7],[Text8],[Mode] FROM [dbo].[Aims_DataCapture] where barcode=@Barcode`, function (err, data) {
+            ,[BrExtra2],[Drop1],[Drop2],[Drop3],[Text1],[Text2],[Text3],[Text4],[Text5],[Text6],[Text7],[Text8],[Mode] FROM [dbo].[Aims_DataCapture] where barcode=@Barcode`, function (err, data) {
                 if (err) console.log(err)
                 if (data == undefined) {
                     return res.status(400).send({ 'error': "An error occured" });
                 }
                 var resultLength = Object.values(data.recordset).length;
-                console.log(resultLength)
+                // console.log(resultLength)
                 if (resultLength == 0)
                     return res.status(404).json({ 'error': 'Data not found' })
 
